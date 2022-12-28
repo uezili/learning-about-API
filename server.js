38a09e956ff2006ec12b342ffe43115b4e9a251f
Wesley Maciel <weslmaciel@gmail.com>
2022-12-28 17:39:53 -0300

    Getting data file to use in server file.

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const app = express();
 
+const app = express();
+const data = require("./data.json")
+
+// verbos HTTP
+// GEt: Receber dados de um Resource.
+// POST: Enviar dados ou informações para serem processados por um Resource.
+// PUT: Atualizar dados de urrl Resource.
+// DELETE: Deletar um Resource.
+
+// ----------------------/ENDPOINT
+// http://localhost:3000/clients
+// -------------URI------------
 
 
 
