d04a9ef71a9c40435b9c1c6493d64dd251189e6b
Wesley Maciel <weslmaciel@gmail.com>
2022-12-28 17:41:10 -0300

    Create a responses for make resquests on server.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const data = require("./data.json")
+const data = require('./data.json');
 
 // verbos HTTP
 // GEt: Receber dados de um Resource.
@@ -13,7 +13,45 @@ const data = require("./data.json")
 // http://localhost:3000/clients
 // -------------URI------------
 
+app.use(express.json());
 
+app.get('/clients', function (req, res) {
+	res.json(data);
+});
+
+app.get('/clients/:id', function (req, res) {
+	const { id } = req.params;
+	const client = data.find((cli) => cli.id == id);
+
+	if (!client) return res.status(204).json();
+	res.json(client);
+});
+
+app.post('/clients', function (req, res) {
+	const { name, email } = req.body;
+
+	// salve
+
+	res.json({ name, email });
+});
+app.put('/clients/:id', function (req, res) {
+	const { id } = req.params;
+	const client = data.find((cli) => cli.id == id);
+
+	if (!client) return res.status(204).json();
+
+	const { name } = req.body;
+
+	client.name = name;
+
+	res.json(client);
+});
+app.delete('/clients/:id', function (req, res) {
+	const { id } = req.params;
+	const clientsFiltered = data.filter((client) => client.id != id);
+
+	res.json(clientsFiltered);
+});
 
 app.listen(3000, function () {
 	console.log('Server is running!');
